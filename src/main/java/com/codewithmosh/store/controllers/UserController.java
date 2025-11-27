2bca897b0ffdbbd2a49042056c726495c65bb1ad
Narges Abbasi <nargesabbasi2976@gmail.com>
2025-11-27 17:26:03 +0330

    Building the shopping cart API

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,104 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.dtos.ChangePasswordRequest;
+import com.codewithmosh.store.dtos.RegisterUserRequest;
+import com.codewithmosh.store.dtos.UpdateUserRequest;
+import com.codewithmosh.store.dtos.UserDto;
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.mappers.UserMapper;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @GetMapping
+//    public Iterable<UserDto> getAllUsers(@RequestHeader(required = false, name = "x-auth-token") String authToken, @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy)
+    public Iterable<UserDto> getAllUsers(@RequestHeader(required = false, name = "x-auth-token") String authToken, @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy) {
+        System.out.println(authToken);
+        if (!Set.of("name", "email").contains(sortBy))
+            sortBy = "name";
+        return userRepository.findAll(Sort.by(sortBy))
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    @RequestMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+//        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return  ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @RequestMapping
+    public ResponseEntity<?> registerUser(@RequestBody RegisterUserRequest request
+    , UriComponentsBuilder uriBuilder) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body(Map.of("email", "Email is already registered."));
+        }
+        var user = userMapper.toEntity(request);
+        userRepository.save(user);
+        var userDto = userMapper.toDto(user);
+        var uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(userDto);
+
+    }
+
+    public ResponseEntity<UserDto> updateUser(@PathVariable(name = "id") long id,
+                              @RequestBody UpdateUserRequest request) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        userMapper.update(request, user);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(userMapper.toDto(user));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    public ResponseEntity<Void> changePassword(@PathVariable Long id,
+                                               @RequestBody ChangePasswordRequest request) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (!user.getPassword().equals(request.getOldPassword())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
---
+++ target/generated-sources/annotations/com/codewithmosh/store/mappers/ProductMapperImpl.java
@@ -0,0 +1,68 @@
+package com.codewithmosh.store.mappers;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Category;
+import com.codewithmosh.store.entities.Product;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-11-24T21:57:47+0330",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 25 (Oracle Corporation)"
+)
+@Component
+public class ProductMapperImpl implements ProductMapper {
+
+    @Override
+    public ProductDto toDto(Product product) {
+        if ( product == null ) {
+            return null;
+        }
+
+        ProductDto productDto = new ProductDto();
+
+        productDto.setCategoryId( productCategoryId( product ) );
+        productDto.setId( product.getId() );
+        productDto.setName( product.getName() );
+        productDto.setPrice( product.getPrice() );
+        productDto.setDescription( product.getDescription() );
+
+        return productDto;
+    }
+
+    @Override
+    public Product toEntity(ProductDto productDto) {
+        if ( productDto == null ) {
+            return null;
+        }
+
+        Product.ProductBuilder product = Product.builder();
+
+        product.id( productDto.getId() );
+        product.name( productDto.getName() );
+        product.description( productDto.getDescription() );
+        product.price( productDto.getPrice() );
+
+        return product.build();
+    }
+
+    @Override
+    public void update(ProductDto productDto, Product product) {
+        if ( productDto == null ) {
+            return;
+        }
+
+        product.setName( productDto.getName() );
+        product.setDescription( productDto.getDescription() );
+        product.setPrice( productDto.getPrice() );
+    }
+
+    private Byte productCategoryId(Product product) {
+        Category category = product.getCategory();
+        if ( category == null ) {
+            return null;
+        }
+        return category.getId();
+    }
+}
