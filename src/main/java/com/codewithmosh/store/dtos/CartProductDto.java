ebd75ed7655890bdc5cc7290ffbdfb71127603ac
Narges Abbasi <nargesabbasi2976@gmail.com>
2025-11-28 16:25:42 +0330

    Adding a product to the cart

---
+++ src/main/java/com/codewithmosh/store/dtos/CartProductDto.java
@@ -0,0 +1,9 @@
+package com.codewithmosh.store.dtos;
+
+import java.math.BigDecimal;
+
+public class CartProductDto {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
