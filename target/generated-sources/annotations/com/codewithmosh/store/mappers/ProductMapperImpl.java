43a2d58878dc5b0f3f98a4329ebe8f9ca97dca3d
Narges Abbasi <nargesabbasi2976@gmail.com>
2025-12-24 16:03:08 +0330

    updating order status

--- target/generated-sources/annotations/com/codewithmosh/store/mappers/ProductMapperImpl.java
+++ target/generated-sources/annotations/com/codewithmosh/store/mappers/ProductMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-12-17T14:43:00+0330",
+    date = "2025-12-24T15:53:45+0330",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25 (Oracle Corporation)"
 )
 @Component
