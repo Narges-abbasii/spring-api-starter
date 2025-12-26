4f42cb829cbbddc07301856c7e0039c2094fca71
Narges Abbasi <nargesabbasi2976@gmail.com>
2025-12-26 15:59:22 +0330

    populating the database

--- target/generated-sources/annotations/com/codewithmosh/store/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/codewithmosh/store/mappers/UserMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-12-26T13:36:09+0330",
-    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
+    date = "2025-12-26T15:35:43+0330",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.17 (Microsoft)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
