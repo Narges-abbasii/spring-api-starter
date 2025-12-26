d139c5602b7fcc9a50248622d530024fc9d94ed5
Narges Abbasi <nargesabbasi2976@gmail.com>
2025-12-26 21:25:31 +0330

    Organizing code by features and fix the bug in StripePaymentGateway

---
+++ src/main/java/com/codewithmosh/store/users/UserDto.java
@@ -0,0 +1,17 @@
+package com.codewithmosh.store.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime createdAt;
+//    @JsonInclude(JsonInclude.Include.NON_NULL)
+//    private String phoneNumber;
+
+}
